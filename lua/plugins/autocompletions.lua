c6c0a66368c5a1a40ab1dc3a1987bd22522de81f
smartiano24 <smartiano24@yahoo.com>
2025-12-02 22:15:50 +0100

    rewrote deprecated typescript lsp

--- lua/plugins/autocompletions.lua
+++ lua/plugins/autocompletions.lua
@@ -27,7 +27,17 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<CR>"] = cmp.config.disable,
         ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-j>"] = cmp.mapping(function(fallback) end, { "i", "s" }),
+        ["<C-j>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       })
     end,
   },
