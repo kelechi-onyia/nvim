b5d7c6b972dcb64b9a57c6cb11002a7847caf753
smartiano24 <smartiano24@yahoo.com>
2025-12-03 00:14:45 +0100

    disable markdown lsp header background highlighting

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,25 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Remove markdown header highlighting
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- Clear highlighting for all markdown headers
+    vim.cmd([[
+      highlight clear @markup.heading.1.markdown
+      highlight clear @markup.heading.2.markdown
+      highlight clear @markup.heading.3.markdown
+      highlight clear @markup.heading.4.markdown
+      highlight clear @markup.heading.5.markdown
+      highlight clear @markup.heading.6.markdown
+      highlight clear markdownH1
+      highlight clear markdownH2
+      highlight clear markdownH3
+      highlight clear markdownH4
+      highlight clear markdownH5
+      highlight clear markdownH6
+    ]])
+  end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,9 @@
 
 -- Display currently opened file path at the top right handside
 vim.opt.winbar = "%=%m %f"
+
+-- Disable colorcolumn (line-length visual hints)
+vim.opt.colorcolumn = ""
+
+-- Disable textwidth (automatic line breaks)
+vim.opt.textwidth = 0
